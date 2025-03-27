c08206817cfcd1ddb5ee3604f54a21c7796c549b
Pablo <114429284+pacerim@users.noreply.github.com>
2025-03-27 16:26:58 +0100

    Add files via upload

---
+++ WhatsappMessenger/WhatsappMessager.py
@@ -0,0 +1,8 @@
+import pywhatkit as pwk
+    
+# Define the recipient's phone number (with country code) and the message
+phone_number = "+34662202145"
+message = "Hola amor, soy Pablo desde el ordenador"
+
+pwk.sendwhatmsg(phone_number, message, 16,7 )
+# Send the message
